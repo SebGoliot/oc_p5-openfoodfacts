851bd87664050c5dc276396ffd5a82f87660a0eb
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2020-08-18 21:35:56 +0200

    Create db.sql

---
+++ src/db.sql
@@ -0,0 +1,31 @@
+DROP DATABASE `openfoodfacts`;
+CREATE DATABASE IF NOT EXISTS  `openfoodfacts`;
+USE `openfoodfacts`;
+
+-- Categories table
+CREATE TABLE IF NOT EXISTS `Categories` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(64) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+-- Products table
+CREATE TABLE IF NOT EXISTS `Products` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+	`name` VARCHAR(64) NOT NULL,
+	`category` INT UNSIGNED NOT NULL,
+	`nutriscore` INT UNSIGNED NOT NULL,
+	`stores` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`category`) REFERENCES `Categories`(`id`)
+);
+
+-- Favorites table
+CREATE TABLE IF NOT EXISTS `Favorites` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`product_id` INT UNSIGNED NOT NULL,
+	`substitued_id` INT UNSIGNED NOT NULL,
+	PRIMARY KEY (`id`),
+    FOREIGN KEY (`product_id`) REFERENCES `Products`(`id`),
+    FOREIGN KEY (`substitued_id`) REFERENCES `Products`(`id`)
+);
