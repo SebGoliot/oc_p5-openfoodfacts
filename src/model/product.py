b21413048086082b7cd4a300fba6979574989376
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2020-09-02 22:02:42 +0200

    Update Product class
    Fixed an arg type and added a constructor for db payloads

--- src/model/product.py
+++ src/model/product.py
@@ -1,12 +1,20 @@
+from typing import Tuple
 
 
 class Product():
 
-    def __init__(self, code: int, name: str, stores: str, grade: int):
+    def __init__(self, code: int, name: str, stores: str, grade: str):
         self.code = code
         self.name = name
         self.stores = stores
         self.grade = grade
+
+    @classmethod
+    def from_db_payload(cls, payload: Tuple[int, str, str, str]):
+        """Create a Product object from a db row payload
+        Should be a tuple containing 4 items: code, name, stores and grade
+        """
+        return cls(*payload)
 
     def __repr__(self):
         return f'#{self.code}:{self.name} @{self.stores} ^{self.grade}'
